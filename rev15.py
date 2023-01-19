cc6c63fb2f9ff0211c46e3d588b99283f8576448
HelryMCavalcanteC <105950260+helryMCavalcante@users.noreply.github.com>
2023-01-19 17:31:38 -0300

    Add files via upload

---
+++ rev15.py
@@ -0,0 +1,7 @@
+#frase = str(input('Digite uma frase: ')).strip()
+#junto = ''.join(frase.split())
+#for letra in junto[::-1]:
+#    print(f'{letra.upper()}',end='')
+
+palavra = str(input('infrome uma palavra'))
+print(palavra[::-1])
