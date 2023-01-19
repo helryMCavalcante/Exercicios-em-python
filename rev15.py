1ea787224d61f79c639bc9dfc0636ed2b1da8412
HelryMCavalcanteC <105950260+helryMCavalcante@users.noreply.github.com>
2023-01-19 20:20:47 -0300

    Delete rev15.py

--- rev15.py
+++
@@ -1,7 +0,0 @@
-#frase = str(input('Digite uma frase: ')).strip()
-#junto = ''.join(frase.split())
-#for letra in junto[::-1]:
-#    print(f'{letra.upper()}',end='')
-
-palavra = str(input('infrome uma palavra'))
-print(palavra[::-1])
